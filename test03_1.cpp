b34ca4b1ee60d2b572ac73ea858f5dac4f28ef66
Aee-rgb <109760203+Aee-rgb@users.noreply.github.com>
2025-01-20 13:39:20 +0400

    Add files via upload

--- test03_1.cpp
+++ test03_1.cpp
@@ -1,1 +1,36 @@
-main () {}
+#include <iostream>
+
+class Car {
+    public:
+	  Car();
+	  Car (int nawP, double nawV);
+	  double X;
+	  int P;
+	  double V;
+};
+
+Car::Car() {
+      X = 0;
+	  }
+	  
+ main (){
+    using namespace std;
+	cout <<"\nWork by Aleksandr Uponavichs\n";
+	
+	Car car;
+	
+	cout << "\nCar X:"  << car.X;
+	cout << "\nLine of car:"  << car.P;
+	cout << "\nSpeed of car:"  << car.V;
+	
+	
+	car.X = 150;
+	car.P = 4;
+	car.V = 70;
+	
+	cout << "\n\nCar X:"  << car.X;
+	cout << "\nLine of car:"  << car.P;
+	cout << "\nSpeed of car:"  << car.V;
+	
+  return 0;
+}
